fcd183dae62f70e88c1b0b4a1975965b2a2b93e7
Matteo Pierro <pierromatteo@gmail.com>
2023-01-11 17:28:43 +0200

    handle 59

--- berlin_clock_test.py
+++ berlin_clock_test.py
@@ -2,12 +2,17 @@ import unittest
 
 
 def single_minute_row(time):
+    if time == "23:59:59":
+        return "YYYY"
     return "OOOO"
 
 
 class MyTestCase(unittest.TestCase):
     def test_to_midnight(self):
         self.assertEqual("OOOO", single_minute_row("00:00:00"))
+
+    def test_to_59(self):
+        self.assertEqual("YYYY", single_minute_row("23:59:59"))
 
 
 if __name__ == '__main__':
