b0b624ff84c78cdfd1890f34333e76e3ca33528f
Matteo Pierro <pierromatteo@gmail.com>
2023-01-11 17:35:33 +0200

    extract single minute

--- berlin_clock_test.py
+++ berlin_clock_test.py
@@ -9,6 +9,10 @@ def single_minute_row(time):
     return "OOOO"
 
 
+def extract_single_minute(time):
+    return int(time.split(":")[1])
+
+
 class MyTestCase(unittest.TestCase):
     def test_to_midnight(self):
         self.assertEqual("OOOO", single_minute_row("00:00:00"))
@@ -19,6 +23,8 @@ class MyTestCase(unittest.TestCase):
     def test_to_32(self):
         self.assertEqual("YYOO", single_minute_row("12:32:00"))
 
+    def test_parse_time_to_single_minute(self):
+        self.assertEqual(32, extract_single_minute("12:32:00"))
 
 if __name__ == '__main__':
     unittest.main()
