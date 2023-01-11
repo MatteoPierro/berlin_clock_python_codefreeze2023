699092df8bf0be1d5480ec59ff57be3941428fa7
Matteo Pierro <pierromatteo@gmail.com>
2023-01-11 23:04:15 +0200

    translate single hours block

--- berlin_clock_test.py
+++ berlin_clock_test.py
@@ -58,8 +58,10 @@ def convert_to_berlin_clock(time):
 
 def berlin_to_digital(berlin_clock):
     five_hours = berlin_clock[1:5]
-    number_of_hours = str(five_hours.count("R") * 5).rjust(2, "0")
-    return f"{number_of_hours}:00:00"
+    single_hours = berlin_clock[5:9]
+    number_of_hours = five_hours.count("R") * 5 + single_hours.count("R")
+    hours = str(number_of_hours).rjust(2, "0")
+    return f"{hours}:00:00"
 
 
 class MyTestCase(unittest.TestCase):
@@ -120,6 +122,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual("05:00:00", berlin_to_digital("YROOOOOOOOOOOOOOOOOOOOOO"))
         self.assertEqual("10:00:00", berlin_to_digital("YRROOOOOOOOOOOOOOOOOOOOO"))
         self.assertEqual("15:00:00", berlin_to_digital("YRRROOOOOOOOOOOOOOOOOOOO"))
+        self.assertEqual("01:00:00", berlin_to_digital("Y000OROOOOOOOOOOOOOOOOOO"))
 
 
 if __name__ == '__main__':
